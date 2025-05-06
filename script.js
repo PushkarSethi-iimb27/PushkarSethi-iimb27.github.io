8cf18d0589154fe35d1ff0082e0fc5a5becfdf58
PushkarSethi-iimb27 <anshu.sethi1981@gmail.com>
2025-05-06 22:35:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add some simple interactivity here if you want
+// Example: Simple alert when clicking a menu item
+
+document.querySelector('.menu-img').addEventListener('click', function() {
+  alert('Enjoy our delicious menu!');
+});
